3c964b92be853823e67ca4288cbffa12f717a9dc
Yuvraj-max222 <srivastavyuvraj36@gmail.com>
2025-05-12 23:40:22 +0530

    RecFac

--- Q4.c
+++ Q4.c
@@ -1,28 +1,19 @@
-// WACP to find whether a number is Armstrong or not by using while and if statement.
+// WACP using a recursive function to find the factorial of a number.
 #include<stdio.h>
-int main() {
-    int n, originalNum, remainder, result = 0, digits = 0, power, i;
-    printf("Enter an integer: ");
+#include<conio.h>
+int fact(int n)
+{
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * fact(n - 1);
+}
+int main()
+{
+    int n;
+    printf("Enter a number to find its factorial: ");
     scanf("%d", &n);
-    originalNum = n;
-    while (originalNum != 0) {
-        originalNum /= 10;
-        ++digits;
-    }
-    originalNum = n;
-    while (originalNum != 0) {
-        remainder = originalNum % 10;
-        power = 1;
-        for (i = 0; i < digits; i++) {
-            power *= remainder;
-        }
-        result += power;
-        originalNum /= 10;
-    }
-    if (result == n) {
-        printf("%d is an Armstrong number.\n", n);
-    } else {
-        printf("%d is not an Armstrong number.\n", n);
-    }
+    printf("Factorial of %d is %d", n, fact(n));
+    getch();
     return 0;
 }
